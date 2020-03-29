1f1e3842bb65a35f2f8e83cd4daf528bf3e54fc5
Martin <mob010@alumnos.ucn.cl>
2020-03-29 20:46:56 -0300

    Feat(ChatDisc): Create MainActivity and activity_main.xml,add code for menu.

--- src/main/java/cl/ucn/disc/dsm/chatdisc/MainActivity.java
+++ src/main/java/cl/ucn/disc/dsm/chatdisc/MainActivity.java
@@ -1,13 +1,48 @@
 package cl.ucn.disc.dsm.chatdisc;
 
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+  TextView username;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setTitle("");
+
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu, menu);
+    return true;
+  }
+
+  //base de datos logout 3 14:41
+
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()){
+      case R.id.logout:
+        startActivity(new Intent(MainActivity.this, StartActivity.class));
+        finish();
+        return true;
+
+    }
+    return false;
   }
 }
