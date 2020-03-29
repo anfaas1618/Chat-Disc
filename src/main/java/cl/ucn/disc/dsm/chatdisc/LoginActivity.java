435b3ca3cff569b71807620447e3d889cf2b65bd
Martin <mob010@alumnos.ucn.cl>
2020-03-29 20:43:54 -0300

    Feat(ChatDisc): Create LoginActivity and activity_login.xml

---
+++ src/main/java/cl/ucn/disc/dsm/chatdisc/LoginActivity.java
@@ -0,0 +1,53 @@
+package cl.ucn.disc.dsm.chatdisc;
+
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+  MaterialEditText email,password;
+  Button btn_login;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setTitle("Register");
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    //auth base de datos?
+
+    email = findViewById(R.id.email);
+    password = findViewById(R.id.password);
+    btn_login = findViewById(R.id.btn_login);
+
+    btn_login.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String txt_email = email.getText().toString();
+        String txt_password = password.getText().toString();
+        if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
+          Toast.makeText(LoginActivity.this, "All fileds are required", Toast.LENGTH_SHORT).show();
+        }else {
+          Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+          startActivity(intent);
+          finish();
+          //else restriccion base de datos 2 21:55
+        }
+
+      }
+    });
+  }
+}
