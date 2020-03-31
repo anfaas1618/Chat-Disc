3e1727a44f5f4b11e819c4d933d2841a91f58100
Martin <mob010@alumnos.ucn.cl>
2020-03-31 07:53:13 -0300

    feat(Adapter):add UserAdapter and user_item.xml

---
+++ src/main/java/cl/ucn/disc/dsm/chatdisc/Adapter/UserAdapter.java
@@ -0,0 +1,88 @@
+package cl.ucn.disc.dsm.chatdisc.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import cl.ucn.disc.dsm.chatdisc.Model.User;
+import cl.ucn.disc.dsm.chatdisc.R;
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+  private Context mContext;
+  private List<User> mUsers ;
+
+
+
+
+
+  public UserAdapter(Context mContext, List<User> mUsers, boolean ischat) {
+    this.mUsers = mUsers;
+    this.mContext = mContext;
+
+
+  }
+
+  @NonNull
+  @Override
+  public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+    return new UserAdapter.ViewHolder(view);
+
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    User user = mUsers.get(position);
+    holder.username.setText(user.getUsername());
+    if(user.getImageURL().equals("default")){
+      holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+    }else{
+      Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
+    }
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return mUsers.size();
+  }
+
+  public class ViewHolder extends RecyclerView.ViewHolder{
+
+    public TextView username;
+    public ImageView profile_image;
+
+
+    public ViewHolder(@NonNull View itemView) {
+      super(itemView);
+
+      username = itemView.findViewById(R.id.username);
+      profile_image = itemView.findViewById(R.id.profile_image);
+
+
+    }
+  }
+
+
+
+
+}
